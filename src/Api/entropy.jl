58d9105a25f36ef04fdfed2656b88838d88475a2
josePereiro <jose.pereiro.personal@gmail.com>
2023-03-14 18:15:51 -0400

    WIP - .gitignore

--- src/Api/entropy.jl
+++ src/Api/entropy.jl
@@ -7,12 +7,18 @@ function entropy(f::Function, enet::EchelonMetNet, fbox::BoxGrid)
     walk_polytope(enet, fbox) do vf
         fi = f(vf)
         Σfi += fi
+        # @show fi
         Σfi_logfi += fi * log(fi)
     end
-    
+    # @show Σfi
+    # @show Σfi_logfi
     ΔV = prod(fbox.dims_steps)
     Σfi_ΔV = Σfi * ΔV
     Σfi_logfi_ΔV = Σfi_logfi * ΔV
     
     return log(Σfi_ΔV) - inv(Σfi_ΔV) * Σfi_logfi_ΔV
 end
+
+_one(x) = one(eltype(x))
+entropy(enet::EchelonMetNet, fbox::BoxGrid) = 
+    entropy(_one, enet, fbox)
